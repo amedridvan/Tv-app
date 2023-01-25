28fafe3e51846595b6c41a6306358406ea120e98
osmanbabaapp <osmanbabaapp@gmail.com>
2023-01-25 17:44:34 +0300

    first commit

---
+++ src/layouts/Header/Header.tsx
@@ -0,0 +1,14 @@
+import Home from '@/pages'
+import React from 'react'
+import Head from 'next/head'
+
+
+const Header = () => {
+  return (
+    <>
+     <h1 className="w-full h-10 bg-orange-500 text-center uppercase font-bold text-4xl hover:text-6xl hover:text-black  hover:h-20">tv app for teaching</h1>
+    </>
+  )
+}
+
+export default Header 
