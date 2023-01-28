b8696f4d3fd0170eda7ea3ca6bd0d9f34e0902d3
osmanbabaapp <osmanbabaapp@gmail.com>
2023-01-28 14:40:34 +0300

    detiles page

--- src/pages/detiles/[media]/[id].tsx
+++ src/pages/detiles/[media]/[id].tsx
@@ -1,54 +1,97 @@
 import { GetServerSideProps } from "next";
 import axios from "axios";
 import React from 'react'
-import {img_500 ,unavailable} from "../../../confing"
+import {img_300 ,img_500 ,unavailable} from "../../../confing"
 import { Button } from "@mui/material";
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import AliceCarousel from "react-alice-carousel";
+import { EffectCoverflow, Pagination, Swiper } from "swiper";
 
 interface Props {
-  items: [];
+  items: []; 
+  vidLink :string;
+  casts: [];
 }
-interface Props1{
-  vid:[];
-}
-const Detiles = ( props:Props,props1:Props1) => {
-  const {items} = props
-  const {vid} = props1
-  const ImageURL =  items.poster_path ? img_500 + items.poster_path: unavailable;
+
+const Detiles = ( props:Props) => {
+  const responsive = {
+    0: {
+      items: 3,
+    },
+    512: {
+      items: 5,
+    },
+    1024: {
+      items: 7,
+    },
+  };
+  const handleDragStart = (e) => e.preventDefault();
+  const items = props.items
+  const vidLink = props.vidLink
+  const casts =props.casts
+
+  console.log('casts', casts)
+
+
+  console.log('vidLink', vidLink)
   const ImageURL2 =  items.backdrop_path ? img_500 + items.backdrop_path: unavailable;
-  console.log(items)
   return (
-    
     <>
-   { items &&( <div style={{
+   {items && ( <div style={{
      scrollbarWidth :"none"
    }} 
-   className='bg-gray-600 w-full
+   className='bg-gray-600 flex flex-col justify-between h-[100%] w-[100%] overflow-y-scroll 
     '>
       {
-            <><img className='rounded-xl object-contain w-full h-full'
+            <>
+            <img className='rounded-xl object-contain w-full h-full'
             alt="a"
             src={ImageURL2} />
-            <div className='flex flex-col justify-between h-full w-full  '>
-              <span className='text-center'>
+            <div className='flex flex-col p-[10p] w-[95%] h-[90%] font-serif justify-evenly font-semibold   '>
+              <span className='flex justify-center items-center h-[12%] text-xl pb-3 '>
                 {items.name || items.title}
                 (
                   {( items.first_air_date || items.release_date || "-----").substring(0,4)}
                 )
               </span> 
               <i className='text-center pb-3'> {items.tagline}</i> 
-              <span className=''>
+              <span  style={{
+                scrollbarWidth : "thin"
+              }} className='flex h-[40%] overflow-y-scroll p-[15px] rounded-3xl shadow-black shadow-lg text-justify m-6 '>
                     {items.overview}
                   </span>
-              <Button variant="text"
-              startIcon={<YouTubeIcon />}
-              target="_blank"
-              href= {`https://www.youtube.com/watch?v${vid}`}
-              > ahmed</Button>
+                  <div>
+           
+                  </div>
+        <div>   
+        <h2>Co-Star Informations</h2>
+        
+        {casts.map((item: any) => {
+          return <div>
+            < img src={`${img_500}${item.profile_path}`} alt="" />
+          </div>
+        } )}   
+        </div> 
+      
+            <Button className='ml-8'
+             variant="contained"
+             startIcon={<YouTubeIcon />}
+             color="secondary"
+             target="__blank"
+              href= {`https://www.youtube.com/watch?v=${vidLink}`}
+              > watch video </Button>
+              <Button className='ml-8 mt-4'
+             variant="contained"
+             startIcon={<YouTubeIcon />}
+             color="primary"
+             target="_self"
+              href= "/"
+              > Back to main </Button>
             </div>
+             
             </>
       }
-    </div> )}
+    </div>)}
     </>
   )
 }
@@ -62,15 +105,19 @@ const { data: res  } = await axios.get(
   const { data: res1  } = await axios.get(
     `https://api.themoviedb.org/3/${params.media}/${params.id}/videos?api_key=c76f21405a6fbe2e74354773617a04b8&language=en-US`
   );
- 
-  console.log('res', res)
-  //console.log("data :>>", res);
+  const { data: res2  } = await axios.get(
+    `https://api.themoviedb.org/3/${params.media}/${params.id}/credits?api_key=c76f21405a6fbe2e74354773617a04b8&language=en-US`
+  );
+  console.log("data :>>", await res1.results[0]?.key);
+
+  console.log('res2.cast >>', res2.cast)
 
   return {
   
     props: {
       items: await res ,
-      vidLinl :await res1.results[0]
+      vidLink :await res1.results[0]?.key,
+      casts: await res2.cast 
     },
   };
 };
